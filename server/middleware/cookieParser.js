3546aba795a3bf1c111e517a27d52c444e7fe234
DominicMonares <dominicsmonares@gmail.com>
2021-10-27 16:52:16 -0700

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,19 @@
 const parseCookies = (req, res, next) => {
+  var object = {};
+  var cookies = req.headers.cookie;
+
+  if (cookies) {
+    cookies = cookies.split('; ');
+    cookies.forEach(cookie => {
+      var [key, value] = cookie.split('=');
+      object[key] = value;
+    });
+
+    req.cookies = object;
+  }
+
+  next();
 };
+
 
 module.exports = parseCookies;
